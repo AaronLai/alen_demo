13cb381eba225547ed7b1b6fd25211c602a05170
Aaron Lai <beckwo1015@gmail.com>
2019-04-16 03:22:34 +0800

    added competitor

--- pink-desh_files/app.js
+++ pink-desh_files/app.js
@@ -36,7 +36,4 @@ var monthNames = [ "January", "February", "March", "April", "May", "June",
     "July", "August", "September", "October", "November", "December" ];
 var week=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]; 
 
-document.getElementById("para1").innerHTML = hours+"."+minutes+ampm;
-document.getElementById("para2").innerHTML = week[dayname];
-document.getElementById("para3").innerHTML = day+" "+monthNames[month]+" "+year;
 });   
